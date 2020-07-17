292bbb56abfa46a1993cf8fd8c7a3c60327b7e10
Natalia Galán <n8galan@gmail.com>
2020-07-17 00:01:58 +0000

    Automatically backed up by Learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -40,12 +40,15 @@ def hit?(card_total)
   prompt_user
  if get_user_input == 'h'
    card_total += deal_card
+   
+   unless get_user_input == 'h' || 's'
+     invalid_command
+     prompt_user
+   end
+   
  end
 
-  unless get_user_input == 'h' || 's'
-    invalid_command
-    prompt_user
-  end
+
 
    card_total
 end
